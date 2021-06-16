23859fd8ad7acc6519c9137c19f0ce13c26de200
Mugecu <forwork_ivan@tut.by>
2021-06-16 12:50:17 +0300

    Создание инфраструктуры проекта приложения

--- TestStrategyWithTest/TestStrategyWithTestTests.cs
+++ TestStrategyWithTest/TestStrategyWithTestTests.cs
@@ -10,15 +10,6 @@ namespace TestStrategyWithTest
     class TestStrategyWithTestTests
     {
         [Test]
-        public void EmptyArgymentInReadClass()
-        {
-            User user = new User();
 
-            Read read = new Read();
-
-            string name = read.DoRead(user.name);
-            
-            Assert()
-        }
     }
 }
