c91c2d640b503e2066bd33ee6de3227976fd429d
Mugecu <forwork_ivan@tut.by>
2021-06-16 12:40:58 +0300

    Добавьте файлы проекта.

---
+++ TestStrategyWithTest/TestStrategyWithTestTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace TestStrategyWithTest
+{
+    [TestFixture]
+    class TestStrategyWithTestTests
+    {
+        [Test]
+        public void EmptyArgymentInReadClass()
+        {
+            User user = new User();
+
+            Read read = new Read();
+
+            string name = read.DoRead(user.name);
+            
+            Assert()
+        }
+    }
+}
