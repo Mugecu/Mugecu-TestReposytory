df5f0769fa1e448e0173e29d572dff0d679b65d9
Mugecu <forwork_ivan@tut.by>
2021-06-16 13:53:39 +0300

    Создание класса Work и методов.

---
+++ TestStrategyWithTest/Work.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestStrategyWithTest
+{
+    class Work
+    {
+        public string DoWork(IActioWork actWork)
+        {
+            return actWork.DoJob();
+        }
+    }
+}
